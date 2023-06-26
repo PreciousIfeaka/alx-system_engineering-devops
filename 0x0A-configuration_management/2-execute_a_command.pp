96f4eac2edbdc29ff2b3b399ae8c01404b788b3b
root <root@vagrant.vm>
2023-06-26 12:49:35 +0000

    killed killmenow process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # This manifest kills the killmenow process
 
 exec {'kill_killmenow':
-  command     => 'pkill killmenow',
-  refreshonly => 'true',
-  path        => '/usr/bin',
+  command => 'pkill killmenow',
+  path    => '/usr/bin',
 }
