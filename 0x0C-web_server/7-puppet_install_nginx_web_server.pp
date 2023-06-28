52eee4cf68987895677863a426bd2cb4281cf490
root <root@vagrant.vm>
2023-06-28 07:07:12 +0000

    puppet configuration for nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,12 @@
 
 exec { 'nginx configuration':
   provider => shell,
-  command  => 'apt-get -y update; apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.nginx-debian.html; sed -i "/listen 80 default_server/a rewrite ^/tredirect_me http://www.youtube.com/watch?v=JCxGxK3GZiA permanent;" /etc/nginx/sites-available/default'
+  command  => 'apt-get -y update; apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.nginx-debian.html; sed -i "/listen 80 default_server/a rewrite ^/redirect_me http://www.youtube.com/watch?v=JCxGxK3GZiA permanent;" /etc/nginx/sites-available/default; echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html; sed -i "/listen 80 default_server/a error_page 404 /custom_404.html;" /etc/nginx/sites-available/default; service nginx restart'
 }
+
+
+
+
+echo "Ceci n'est pas une page" > /var/www/html/custom_404.html
+sed -i '/listen 80 default_server/a error_page 404 /custom_404.html;' /etc/nginx/sites-available/default 
+service nginx restart
