fecd5930dfdb3e3dcdb33a570439e3aa843a01f3
root <root@vagrant.vm>
2023-08-07 13:25:12 +0000

    exported to csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""This script uses REST API, accepts an employee ID as an argument and
+retuns inforation about his/her TODO progress
+"""
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    id = argv[1]
+    url1 = f'https://jsonplaceholder.typicode.com/users/{id}'
+    url2 = f'https://jsonplaceholder.typicode.com/todos?userId={id}'
+
+    resp = requests.get(url1)
+    name = resp.json().get("name")
+
+    res = requests.get(url2)
+    res = res.json()
+    if name:
+        lists = []
+        fields = ['id', 'name', 'completed', 'title']
+        filename = f'{id}.csv'
+        csvfile = open(filename, 'w')
+        csvwriter = csv.DictWriter(csvfile, fieldnames = fields)
+
+        for user in res:
+            doneOrNot = user.get('completed')
+            title = user.get('title')
+
+            row = {'id': id, 'name': name, 'completed': doneOrNot,\
+                    'title': title}
+            lists.append(row)
+        csvwriter.writerows(lists)
