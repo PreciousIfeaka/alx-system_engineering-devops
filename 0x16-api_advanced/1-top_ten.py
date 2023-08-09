271d28b7d10088fbdfa2cfaf1e949323ef5f91db
root <root@vagrant.vm>
2023-08-09 03:27:24 +0000

    obtained the first 10 hot topics

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This script queries the reddit api and prints
+the titles of the first 10 host posts for a given subreddit
+"""
+
+import sys
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    with requests.get(url, allow_redirects=False,
+                      params={'limit': 10}) as resp:
+        if resp.status_code == 200:
+            s_list = resp.json().get('data').get('children')
+            for key in s_list:
+                topic = key.get('data').get('title')
+                print(topic)
+        else:
+            print(None)
